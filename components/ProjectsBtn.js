841726e4af4100816fa9064ca255d5747badb4d1
chakorn.san <chakorn.san@axonstech.com>
2024-09-06 20:05:47 +0700

    feat : test gtm

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -1,16 +1,19 @@
+import { sendGAEvent } from "@next/third-parties/google";
 import Image from "next/image";
 import Link from "next/link";
 const ProjectsBtn = () => {
   return (
     <div className="relative inline-flex  group">
-      <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
-      </div>
-      <Link href={'/work'}
+      <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+      <Link
+        href={"/work"}
+        onClick={() => sendGAEvent("event", "buttonClicked", { value: "xyz" })}
         className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 rounded-full focus:none focus:ring-gray-900"
-      >My Projects
+      >
+        My Projects
       </Link>
     </div>
-  )
+  );
 };
 
 export default ProjectsBtn;
